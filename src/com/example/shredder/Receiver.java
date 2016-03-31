dab7ad2d9d39c951f7540b96f354629f2a08ff1c
clock <double7@mail.com>
2016-03-31 18:40:47 +0600

    shreder

---
+++ src/com/example/shredder/Receiver.java
@@ -0,0 +1,32 @@
+package com.example.shredder;
+
+import java.io.IOException;
+
+import android.app.Activity;
+
+public class Receiver implements Runnable {
+	
+	MainActivity activity;
+	
+	public Receiver(Activity activity) {
+		this.activity = (MainActivity)activity;
+	}
+	
+	public void run() {
+		try {
+			String line = activity.getSockIn().readLine();
+			while (line != null) {
+				activity.addMessage(line.trim());
+				activity.runOnUiThread(new Runnable() {
+					public void run() {
+						activity.updateText();
+					}
+				});
+				line = activity.getSockIn().readLine();
+			}
+		} catch (IOException ioex) {
+			// ...
+		}
+	}
+
+}
